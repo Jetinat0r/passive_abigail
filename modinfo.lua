447c6bf11767ed1b30306763f2168e8c9e2bad9c
Jetinat0r <jetin8t0r@gmail.com>
2021-10-24 19:52:24 -0400

    Initial Push
    The entire mod :P

---
+++ modinfo.lua
@@ -0,0 +1,12 @@
+name = "Passive Abigail"
+description = "Makes passive Abigail not attack things ever."
+author = "Jetinat0r"
+version = "1.0.0"
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+dst_compatible = true
+client_only_mod = false
+all_clients_require_mod = true
+
+-- this setting is dumb; this mod is likely compatible with all future versions
+api_version = 10
